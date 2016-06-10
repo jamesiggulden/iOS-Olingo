9540f25f8f90a6bd8b03560ee475aa11c62665c1
Greg Napier <gregnapier@gregs-mbp.lan>
2016-06-10 16:04:51 +0100

    Initial Commit

---
+++ iOS-Olingo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Olingo
+//
+//  Created by Greg Napier on 10/06/2016.
+//  Copyright © 2016 EnergySys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
