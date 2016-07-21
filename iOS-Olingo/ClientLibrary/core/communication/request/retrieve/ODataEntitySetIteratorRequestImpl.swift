e37220f7a8e14caa555909969ea20a7f48f13766
Greg Napier <gregnapier@Gregs-MBP.lan>
2016-07-21 11:46:36 +0100

    Updates to entity set and entity son parsing and binding

---
+++ iOS-Olingo/ClientLibrary/core/communication/request/retrieve/ODataEntitySetIteratorRequestImpl.swift
@@ -0,0 +1,9 @@
+//
+//  ODataEntitySetIteratorRequestImpl.swift
+//  iOS-Olingo
+//
+//  Created by Greg Napier on 20/07/2016.
+//  Copyright © 2016 EnergySys. All rights reserved.
+//
+
+import Foundation
